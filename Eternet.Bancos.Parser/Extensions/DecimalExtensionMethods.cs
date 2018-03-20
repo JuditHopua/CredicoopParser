903b7c7d0defa6a1d6b90784e6391271e3e1a436
Judit Hopua <jhopua@msn.com>
2018-03-20 09:46:06 -0300

    Correction on DecimalExtensionMethods

--- Eternet.Bancos.Parser/Extensions/DecimalExtensionMethods.cs
+++ Eternet.Bancos.Parser/Extensions/DecimalExtensionMethods.cs
@@ -1,11 +1,22 @@
-﻿namespace Eternet.Bancos.Parser.Extensions
+﻿using System;
+
+namespace Eternet.Bancos.Parser.Extensions
 {
     public static class DecimalExtensionMethods
     {
         public static decimal ToDecimal(this string str)
         {
             if (!decimal.TryParse(str, out var result))
-                return 0.00m;
+            {
+                if (str == "")
+                {
+                    return 0.00m;
+                }
+                else
+                {
+                    throw new ArgumentException($"Can't convert {str} to a decimal");
+                }
+            }
             return result;
         }
     }
